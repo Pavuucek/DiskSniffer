b059efc417cbdcb56ca5b41dcc1c959c16dae3cb
Michal Kuncl <michal.kuncl@gmail.com>
2013-05-07 18:12:46 +0200

    pridan SharpCompress pro zpracovani archivu

---
+++ Forms/DlgProgress.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DiskSniffer.Forms
+{
+    partial class DlgProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblStatus.Location = new System.Drawing.Point(0, 35);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(701, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "label1";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progBar
+            // 
+            this.progBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progBar.Location = new System.Drawing.Point(0, 0);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(701, 35);
+            this.progBar.TabIndex = 1;
+            // 
+            // DlgProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 48);
+            this.Controls.Add(this.progBar);
+            this.Controls.Add(this.lblStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DlgProgress";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DlgProgress";
+            this.Load += new System.EventHandler(this.DlgProgressLoad);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progBar;
+
+
+    }
+}
---
+++ Forms/DlgProgress.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiskSniffer.Forms
+{
+    public partial class DlgProgress : Form
+    {
+        public DlgProgress()
+        {
+            InitializeComponent();
+        }
+
+        private void DlgProgressLoad(object sender, EventArgs e)
+        {
+            foreach (var form in Application.OpenForms)
+            {
+                if(form is DlgProgress && form!=this)
+                {
+                    StartPosition = FormStartPosition.Manual;
+                    Top += Height+2;
+                }
+            }
+            lblStatus.Text = string.Format(_progressFormat, progBar.Value, progBar.Maximum);
+        }
+
+        public int MaxProgress
+        {
+            get { return progBar.Maximum; }
+            set
+            {
+                progBar.Minimum = 0;
+                progBar.Maximum = value;
+                progBar.Step = 1;
+            }
+        }
+
+        public int CurrentProgress
+        {
+            get { return progBar.Value; }
+            set { progBar.Value = value; }
+        }
+
+        public void DoProgress()
+        {
+            progBar.PerformStep();
+            lblStatus.Text = string.Format(_progressFormat, progBar.Value, progBar.Maximum);
+            Application.DoEvents();
+        }
+
+        private string _progressFormat;
+
+        public string ProgressFormat
+        {
+            get { return _progressFormat; }
+            set { _progressFormat = value; }
+        }
+    }
+}
